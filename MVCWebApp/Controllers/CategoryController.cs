18681293f909256c74cc556df20cd0e7280b36f3
Ahsan Ahmed <ahsanahmad779@outlook.com>
2025-04-13 06:11:13 +0500

    MVC App

---
+++ MVCWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Data;
+using MVCWebApp.Models;
+
+namespace MVCWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "Name and Display Order cannot be same");
+            //}
+
+            //if(obj.Name != null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Name cannot be test");
+            //}
+
+            if (ModelState.IsValid)
+            {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //If finding by primary key 
+            Category? categoryObj = _db.Categories.Find(id);
+            // //If finding by anyother key
+            //Category? categoryObj1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            // // If finding by any criteria
+            //Category? categoryObj2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+
+            if (categoryObj == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryObj);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category categoryObj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(categoryObj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //If finding by primary key 
+            Category? categoryObj = _db.Categories.Find(id);
+
+            if (categoryObj == null)
+            {
+                return NotFound();
+            }
+            return View(categoryObj);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? categoryObj = _db.Categories.Find(id);
+            if(categoryObj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryObj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
